130a1eabc581dc3f0ac657026f99eb7b1a57fe50
Canel420 <4521@holbertonschool.com>
2022-05-18 13:48:35 -0500

    ss

--- main.c
+++ main.c
@@ -16,12 +16,12 @@ int main(int argc, char **argv)
 
 	if (argc != 2)
 	{
-		fprintf(stderr, "USAGE: monty file");
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE); }
 	fp = fopen(argv[1], "r");
 	if (fp == NULL)
 	{
-		fprintf(stderr, "Error: Can't open file %s", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE); }
 	while (getline(&line, &len, fp) != -1)
 	{
@@ -36,7 +36,7 @@ int main(int argc, char **argv)
 			token = strtok(NULL, " \n\t");
 			if (token == NULL)
 			{
-				fprintf(stderr, "L%d: usage: push integer", line_number);
+				fprintf(stderr, "L%d: usage: push integer\n", line_number);
 				exit(EXIT_FAILURE);
 			}
 			push(&stack, line_number); }
