2e1decf02173ca8665d30182c14c9abc012f24c0
Canel420 <4521@holbertonschool.com>
2022-05-18 09:48:01 -0500

    removed lines

--- main.c
+++ main.c
@@ -1,41 +1,33 @@
 #include "monty.h"
-
+char *token;
 /**
  * main - entry point
  * @argc: number of arguments
  * @argv: array of arguments
  * Return: 0 on success
  */
-
-char *token;
-
 int main(int argc, char **argv)
 {
 	FILE *fp;
 	char *line = NULL;
 	size_t len = 0;
-	ssize_t read = 0;
 	stack_t *stack = NULL;
 	int line_number = 1;
 
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
+		exit(EXIT_FAILURE); }
 	fp = fopen(argv[1], "r");
 	if (fp == NULL)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-	while ((read = getline(&line, &len, fp)) != -1)
+		exit(EXIT_FAILURE); }
+	while (getline(&line, &len, fp) != -1)
 	{
 		token = strtok(line, " \n\t");
 		if (token == NULL)
-		{
 			continue;
-		}
 		if (strcmp(token, "push") == 0)
 		{
 			token = strtok(NULL, " \n\t");
@@ -44,16 +36,12 @@ int main(int argc, char **argv)
 				fprintf(stderr, "L%d: usage: push integer\n", line_number);
 				exit(EXIT_FAILURE);
 			}
-			push(&stack, line_number);
-		}
+			push(&stack, line_number); }
 		else
 		{
-			checker(&stack, line_number);
-		}
-		line_number++;
-	}
+			checker(&stack, line_number); }
+		line_number++; }
 	free_stack(stack);
 	free(line);
 	fclose(fp);
-	return (0);
-}
+	return (0); }
