9463b4adf7cd9fb82a4e0bbd5427d85d7576ddfc
Canel420 <4521@holbertonschool.com>
2022-05-18 19:29:03 -0500

    error fixed

--- 1-opcodes.c
+++ 1-opcodes.c
@@ -73,7 +73,7 @@ void _div(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	tmp = *stack;
